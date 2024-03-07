67a1683eb1fd9872e4d6cad42934b37b2ff39faa
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-03-07 17:25:40 -0500

    Update Lab4.java

--- DataS_Lab4/Lab4.java
+++ DataS_Lab4/Lab4.java
@@ -1,27 +1,28 @@
 package DataS_Lab4;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
-import DataS_Lab4.Stacks.FileCreate;
+
 import DataS_Lab4.Stacks.*;
 import DataS_Lab4.Queue.*;
 
 public class Lab4 {
 
     public static void main(String[] args) {
-
-        /*
-         * Scanner input = new Scanner(System.in);
-         * System.out.println("Enter a String");
-         * String userInput = input.nextLine();
-         * question1(userInput);
-         */
-
-        // question3();
-
-        // (7 * 11) + 2.33 = 79.33
-        // double thing = question4("3 4 5 6 7 + - * 3 / + =");//((3+4) * (5+6)) + 7/3
+        System.out.print("Question 1 = ");
+        question1("Live and Let Live");
+        System.out.println("");
+        System.out.print("question 2 = ");
+        question2();
+        System.out.println("question 3 = ");
+        question3();
+        int[] temp = new int[3];
+        temp[0] = question4("3 4 + =");
+        temp[1] = question4("7 5 + 8 2 - * =");
+        temp[2] = question4("3 4 5 6 7 + - * 3 / + =");
+        System.out.println("Questions 4 answers are \nA = " + temp[0] + "\nB = " + temp[1] + "\nC = " + temp[2]);
+        System.out.println("Question 5");
         question5();
     }
 
@@ -38,198 +39,86 @@ public class Lab4 {
     }
 
     public static void question2() {
+        String dir = System.getProperty("user.dir");
         MyStack<Character> stack = new MyStack<Character>();
-        MyStack<Character> stack2 = new MyStack<Character>();
-        char[] array = new char[10];
-        FileCreate file = new FileCreate();
-        int count = 0;
+        try (FileReader input = new FileReader(dir + "\\DataS_Lab4\\Code.txt");) {
 
-        file.writeToFile("C R Y * S * M T A A * T E *");
-        stack = file.readFile();
-
-        while (!(stack.isEmpty())) {
-            if (stack.pop() == '*') {
-                stack.pop();
-
-                stack2.push(stack.pop());
-                count++;
+            int c;
+            while ((c = input.read()) != -1) {
+                stack.push((char) c);
             }
+            while (!stack.isEmpty()) {
+                char ch = stack.pop();
+                if (ch == '*') {
+                    stack.pop();
+                    System.out.print(stack.pop());
+                }
+            }
+            System.out.println();
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i < count; i++) {
-            array[i] = stack2.pop();
-        }
-
-        String temp = " ";
-        for (int i = 0; i < count; i++) {
-            temp += array[i] + " ";
-        }
-        question1(temp);
     }
 
     public static void question3() {
-        FileCreate file = new FileCreate();
-        file.writeToFile("[()}");
-
+        String dir = System.getProperty("user.dir");
         MyStack<Character> stack = new MyStack<Character>();
-        stack = file.readFile();
-
-        int leftCount = 0, rightCount = 0;
-        while (!(stack.isEmpty())) {
-            char temp = stack.pop();
-            if (temp == '(') {
-                leftCount++;
-            } else if (temp == ')') {
-                rightCount++;
-            } else if (temp == '[') {
-                leftCount++;
-            } else if (temp == ']') {
-                rightCount++;
-            } else if (temp == '{') {
-                leftCount++;
-            } else if (temp == '}') {
-                rightCount++;
+        try (FileReader input = new FileReader(dir + "\\DataS_Lab4\\Code2.txt");) {
+            int c;
+            while ((c = input.read()) != -1) {
+                stack.push((char) c);
             }
-        }
-        if (leftCount > rightCount) {
-            System.out.println("Right parenthesis missing");
-            System.out.println("Extra left parenthesis");
-        } else if (rightCount > leftCount) {
-            System.out.println("Left parenthesis missing");
-            System.out.println("Extra Right parenthesis");
-        } else
-            System.out.println("Left and right parentheses are balance");
-    }
-
-    public static boolean checkIfZero(int value) {
-        if (value == 0) {
-            return false;
-        } else
-            return true;
-    }
-
-    public static double question4(String value) {
-        MyStack<String> stack = new MyStack<String>();
-        MyStack<Integer> answerStack = new MyStack<Integer>();
-        stack.push(value);
-        String[] temp = stack.pop().split(" ");
-        int answerAdd = 0, answerminus = 0, answerMulti = 0;
-        double answerdiv = 0;
-        double finalanswer = 0;
-
-        int value1 = 0, value2 = 0, count = 0;
-
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("+")) {
-                answerAdd += value1 + value2;
-                value1 = 0;
-                value2 = 0;
-            } else if (temp[i].equals("-")) {
-                answerminus += value1 - value2;
-                value1 = 0;
-                value2 = 0;
-            } else if (temp[i].equals("*")) {
-                answerMulti += value1 * value2;
-                value1 = 0;
-                value2 = 0;
-            } else if (temp[i].equals("/")) {
-                value1 = 1;
-                value2 = 1;
-                answerdiv += value1 / value2;
-
-            } else if (temp[i].equals("=")) {
-
-                if (temp[i - 1].equals("+")) {
-                    finalanswer = answerAdd + answerMulti + answerdiv + answerminus;
-                    return finalanswer;
+            int leftCount = 0, rightCount = 0;
+            int roundCountright = 0, roundCountleft = 0, curlcountRight = 0, curlcountLeft = 0, squareCountRight = 0,
+                    squareCountLeft = 0;
+            while (!(stack.isEmpty())) {
+                char temp = stack.pop();
+                if (temp == '(') {
+                    leftCount++;
+                    roundCountleft++;
+                } else if (temp == ')') {
+                    rightCount++;
+                    roundCountright++;
+                } else if (temp == '[') {
+                    leftCount++;
+                    squareCountLeft++;
+                } else if (temp == ']') {
+                    rightCount++;
+                    squareCountRight++;
+                } else if (temp == '{') {
+                    leftCount++;
+                    curlcountLeft++;
+                } else if (temp == '}') {
+                    rightCount++;
+                    curlcountRight++;
                 }
-                if (temp[i - 1].equals("-")) {
-                    finalanswer = answerAdd - answerMulti - answerdiv - answerminus;
-                    return finalanswer;
-                }
-                if (temp[i - 1].equals("*")) {
-                    if (answerMulti == 0) {
-                        answerMulti = 1;
-                    }
-                    if (answerdiv == 0) {
-                        answerdiv = 1;
-                    }
-                    finalanswer = answerAdd * answerMulti * answerdiv * answerminus;
-                    return finalanswer;
-                }
-                if (temp[i - 1].equals("/")) {
-                    if (answerMulti == 0) {
-                        answerMulti = 1;
-                    }
-                    if (answerdiv == 0) {
-                        answerdiv = 1;
-                    }
-                    finalanswer = (answerAdd / answerminus) / (answerdiv / answerMulti);
-                    return finalanswer;
-                }
+            }
+            if (leftCount > rightCount) {
+                System.out.println("Right parenthesis missing");
+                System.out.println("Extra left parenthesis");
+            } else if (rightCount > leftCount) {
+                System.out.println("Left parenthesis missing");
+                System.out.println("Extra Right parenthesis");
+            } else if ((roundCountright < roundCountleft) || (curlcountRight < curlcountLeft)
+                    || (squareCountRight < squareCountLeft)) {
+                System.out.println("Right parenthesis missing");
+                System.out.println("Extra left parenthesis");
+            } else if ((roundCountright > roundCountleft) || (curlcountRight > curlcountLeft)
+                    || (squareCountRight > squareCountLeft)) {
+                System.out.println("Left parenthesis missing");
+                System.out.println("Extra Right parenthesis");
             } else {
-                if (count == 0) {
-                    value1 = Integer.valueOf(temp[i]);
-                    count = 1;
-                } else {
-                    value2 = Integer.valueOf(temp[i]);
-                    count = 0;
-                }
+                System.out.println("Left and right parentheses are balance");
             }
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return 0;
     }
 
-    public static void question5() {
-        FileCreate fileCreate = new FileCreate();
-
+    public static int question4(String value) {
         MyStack<String> stack = new MyStack<String>();
-
-        fileCreate.createFileString();
-        fileCreate.writeToFileString(
-                "Name:Dylan Age:60 AccountNumber:103 Transaction:Account\nName:Dylan2 Age:40 AccountNumber:103 Transaction:other\nName:Dylan3 Age:20 AccountNumber:103 Transaction:Account");
-        stack = fileCreate.readFileString();
-
-        String[] temp = new String[2];
-        String[] temp2 = new String[2];
-        String[] temp3 = new String[2];
-        String[] temp4 = new String[2];
-
-        int i = 0;
-
-        Person[] people = new Person[23409867];
-        while (!(stack.isEmpty())) {
-
-            String currentChar = stack.pop();
-            temp = currentChar.split(":");
-            currentChar = stack.pop();
-            temp2 = currentChar.split(":");
-            currentChar = stack.pop();
-            temp3 = currentChar.split(":");
-            currentChar = stack.pop();
-            temp4 = currentChar.split(":");
- 
-            people[i] = new Person(temp4[1], temp[1], Integer.parseInt(temp3[1]), Integer.parseInt(temp2[1]));
-            i++;
-        }
-
-        Bank bank = new Bank();
-        for (int j = 0; j < people.length; j++) {
-            if(people[j] != null){
-                bank.addQ(people[j]);
-            }
-        }
-        bank.printElderQ();
-        bank.printAccountQ();
-        bank.printotherQ();
-
-    }
-}
-
-/*
-public static int question4(String value) {
-        MyStack<String> stack = new MyStack<String>();
-        stack.push(value);
         int num1 = 0;
         int num2 = 0;
         int result = 0;
@@ -270,7 +159,54 @@ public static int question4(String value) {
                 stack.push(String.valueOf(temp[i]));
             }
         }
-
         return 0;
     }
-*/
+
+    public static void question5() {
+        String dir = System.getProperty("user.dir");
+        MyStack<String> stack = new MyStack<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader(dir + "\\DataS_Lab4\\Today.txt"))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    stack.push(word);
+                }
+            }
+
+            String[] temp = new String[2];
+            String[] temp2 = new String[2];
+            String[] temp3 = new String[2];
+            String[] temp4 = new String[2];
+
+            int i = 0;
+            
+            Person[] people = new Person[10000];
+            while (!(stack.isEmpty())) {
+
+                String currentChar = stack.pop();
+                temp = currentChar.split(":");
+                currentChar = stack.pop();
+                temp2 = currentChar.split(":");
+                currentChar = stack.pop();
+                temp3 = currentChar.split(":");
+                currentChar = stack.pop();
+                temp4 = currentChar.split(":");
+                people[i] = new Person(temp4[1], temp[1], Integer.parseInt(temp3[1]), Integer.parseInt(temp2[1]));
+                i++;
+            }
+            Bank bank = new Bank();
+            for (int j = 0; j < people.length; j++) {
+                if (people[j] != null) {
+                    bank.addQ(people[j]);
+                }
+            }
+            bank.printElderQ();
+            bank.printAccountQ();
+            bank.printotherQ();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
